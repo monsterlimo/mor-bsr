bbb1d3f2552a40ee63aaf0984f1e11af615c60f7
monsterlimo <mattnixon79@googlemail.com>
2023-04-18 08:16:11 +0100

    added in flow for advice v4

--- app/routes/sprint-6.js
+++ app/routes/sprint-6.js
@@ -1,9 +1,38 @@
 const _ = require('lodash')
 
 module.exports = router => {
-    router.post('/sprint-6/start', (req, res) => {
-        res.redirect('/sprint-6/about-the-person/type-of-person')
+    router.post('/sprint-6/start-mor', (req, res) => {
+        req.session.data['enquiry-about'] = "mor";
+        res.redirect('/sprint-6/building-in-scope/prof-is-the-building-occupied')
     })
+
+    router.post('/sprint-6/start-advice', (req, res) => {
+        res.redirect('/sprint-6/about-the-request/request-about')
+    })
+
+    router.post('/sprint-6/about-the-request/request-about', (req, res) => {
+        res.redirect('/sprint-6/about-the-request/complaint-or-advice')
+    })
+
+    router.post('/sprint-6/about-the-request/complaint-or-advice', (req, res) => {
+        res.redirect('/sprint-6/about-the-request/who-are-you')
+    })
+
+    router.post('/sprint-6/about-the-request/who-are-you', (req, res) => {       
+        if (req.session.data['request-about-what'] == "building") {
+            res.redirect('/sprint-6/about-the-building/postcode-lookup')
+        } else {
+            // bsr, other, person
+            res.redirect('/sprint-6/about-the-person/enter-your-name')
+        }
+    })
+
+    router.post('/sprint-6/about-the-building/do-you-know-the-address', (req, res) => {
+        res.redirect('/sprint-6/about-the-building/postcode-lookup')
+    })
+
+
+    /*
 
     router.post('/sprint-6/about-the-person/type-of-person', (req, res) => {
         if (req.session.data['type-of-person'] == "public") {
@@ -56,7 +85,7 @@ module.exports = router => {
             res.redirect('/sprint-6/about-the-person/enter-your-name')
         }
     })
-
+    */
     
 
 
@@ -89,27 +118,39 @@ module.exports = router => {
     })
 
     router.post('/sprint-6/about-the-person/enter-your-contact-details', (req, res) => {
-        if (req.session.data['enquiry-about'] == "mor") {
-            res.redirect('/sprint-6/about-the-person/what-is-your-role')
-        } else if (req.session.data['enquiry-about'] == "complaint") {
-            if (req.session.data['about-a-building'] == "yes") {
-                res.redirect('/sprint-6/about-the-ap/share-details-with-ap')
-            } else {
-                res.redirect('/sprint-6/about-the-complaint/complaint-details')
-            }
+       if (req.session.data['enquiry-about'] == "advice") {
+        if (req.session.data['request-about-what'] == "person") {
+            res.redirect('/sprint-6/about-the-request/ask-a-question-about-who')
         } else {
-            if (req.session.data['about-a-building'] == "yes") {
-                res.redirect('/sprint-6/about-the-person/what-is-your-relationship-to-the-building')
-            } else {
-                res.redirect('/sprint-6/about-the-request/enter-advice-required')
-            }
+            res.redirect('/sprint-6/about-the-request/enter-advice-required')
         }
+       } else {
+        // it's a complaint
+        if (req.session.data['request-about-what'] == "building") {
+            res.redirect('/sprint-6/about-the-complaint/have-you-contacted-the-ap')
+        } else {
+            res.redirect('/sprint-6/about-the-complaint/complain-about-who')
+        }
+        
+       }
     })
 
+    router.post('/sprint-6/about-the-complaint/complain-about-who', (req, res) => {
+        res.redirect('/sprint-6/about-the-complaint/have-you-contacted-the-ap')
+    })
+
+    router.post('/sprint-6/about-the-request/ask-a-question-about-who', (req, res) => {
+        res.redirect('/sprint-6/about-the-request/enter-advice-required')
+    })
+
+    router.post('/sprint-6/about-the-complaint/have-you-contacted-the-ap', (req, res) => {
+        res.redirect('/sprint-6/about-the-complaint/complaint-details')
+    })
+/*
     router.post('/sprint-6/about-the-ap/share-details-with-ap', (req, res) => {
         res.redirect('/sprint-6/about-the-person/what-is-your-relationship-to-the-building')
     })
-
+*/
     // only appears for public
     router.post('/sprint-6/about-the-person/what-is-your-relationship-to-the-building', (req, res) => {
         if (req.session.data['enquiry-about'] == "advice") {
@@ -126,24 +167,9 @@ module.exports = router => {
             }
         }
         
-        /*
-        if (req.session.data['building-resident'] == "yes") {
-            if (req.session.data['enquiry-about'] == "advice") {
-                if (req.session.data['building-region']) {
-                    res.redirect('/sprint-6/building-in-scope/number-of-floors') 
-                } else {
-                    res.redirect('/sprint-6/about-the-request/enter-advice-required') 
-                }
-            } else {
-                res.redirect('/sprint-6/building-in-scope/any-building-work')  
-            }
-             
-        } else {
-            res.redirect('/sprint-6/building-in-scope/is-the-building-occupied')   
-        }
-        */
+        
     })
-
+    
     router.post('/sprint-6/building-in-scope/any-building-work', (req, res) => {
         if (req.session.data['building-region']) {
             res.redirect('/sprint-6/building-in-scope/number-of-floors')   
@@ -249,7 +275,7 @@ module.exports = router => {
     router.post('/sprint-6/supporting-information/review-uploads', (req, res) => {
         if (req.session.data['enquiry-about'] == "complaint") {
             // no complaint goes to AP
-            res.redirect('/sprint-6/about-the-ap/have-you-contacted-the-ap')
+            res.redirect('/sprint-6/about-the-ap/enter-ap-details')
         } else {
             res.redirect('/sprint-6/check-your-answers')
         }
@@ -289,4 +315,5 @@ module.exports = router => {
     router.post('/sprint-6/check-your-answers', (req, res) => {
         res.redirect('/sprint-6/confirmation')
     })
+    
 }
