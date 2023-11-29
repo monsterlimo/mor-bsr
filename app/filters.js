f6c5eb59b26e925f8b62f9f005b7015722ef4094
monsterlimo <mattnixon79@googlemail.com>
2023-11-29 11:01:16 +0000

    updated date
    also added report already submitted page

--- app/filters.js
+++ app/filters.js
@@ -1,2 +1,7 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+
+addFilter('uppercase', function (content) {
+    return content.toUpperCase()
+})
+
